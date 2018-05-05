8d7d64c1042f6ec08c2bf7bd7014f6f9b071bba7
Andrew Pollock <andrewpollock05@gmail.com>
2018-05-05 06:30:19 -0400

    final product

---
+++ javasript.js
@@ -0,0 +1,40 @@
+// Action One: Player clicks on one of four possible characters as his/her Player
+// (a)	Declare variables for each of the following:
+// (ii) the total number of points that each character begins with (Character Strength)
+// (ii) the total number of points that each character will employ on each attack (Character Power)
+// (b)	Declare functions to calculate Character Strength and Character Power for each character
+// (c)	Call the functions in (b) to assign values to the two variables
+// (d)	Log Character Strengths to each character box
+// (e)	Declare function to move clicked Player to “Your Character” container
+// (f)	Call the function in (e)
+// (g)	Declare function to collapse character container
+// (h)	Call the function in (g)
+// (i)	Declare function to un-collapse “Enemies Available to Attack” container
+// (j)	Call the function in (i)
+// (k)	Declare function to move the remaining three characters to the “Enemies Available to Attack” container
+// (l)	Call the function in (k)
+// Action Two: Player clicks on one of the three/two/one remaining character to act as defender:
+// (a)	Declare function to un-collapse Defender container
+// (b)	Call function in (a)
+// (c)	Declare function to move first/second/third character to Defender container
+// (d)	Call function in (b)
+// Action Three: Player clicks on Attack button
+// (a)	Declare variables to calculate each of the following:
+// (i)	The total number of points lost by the Defender on Players attacks
+// (ii)	The total number of points lost by Player on Defenders attacks
+// (b)	Declare function to calculate variables in (a)
+// (c)	Call function in (b)
+// (d)	Log values attributed to variables in (a) and show them in statements indicating how many points each character has attacked with
+// (e)	Declare function to calculate the diminishing Character Strength of each character
+// (f)	Call function in (e)
+// (g)	Log values calculated in (e) and (f) and show them in Player/Defender character boxes
+// (h)	Repeat (c), (d), (f) and (g) until either Player’s Character Strength equals zero
+// (i)	If Defender equals zero first:
+// a.	Hide defender box
+// b.	Report: “you have won; next defender”
+// c.	Go back to Action Two and repeat
+// (j)	If Player equals zero first:
+// a.	Report “your have lost”
+// b.	Un-hide “try again” box
+// c.	Go back to Action One 
+
